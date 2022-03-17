f72f63836d2ca3d50a628e8717854d86fbba1882
anna kartasheva <annakartashevamail@gmail.com>
2022-03-17 21:20:14 +0300

    renamed to UserAccount, created superuser, registered the class in admin

--- payment_handler/handler/models.py
+++ payment_handler/handler/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class UserAccounts(models.Model):
+class UserAccount(models.Model):
     owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
