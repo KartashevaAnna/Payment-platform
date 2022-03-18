2b1c8e41912b35a8267c8af1a394566115f7c111
anna kartasheva <annakartashevamail@gmail.com>
2022-03-18 15:00:28 +0300

    requests rerouted to the application users

--- payment_handler/payment_handler/urls.py
+++ payment_handler/payment_handler/urls.py
@@ -1,10 +1,11 @@
 """payment_handler URL Configuration"""
-
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('', include('handler.urls', namespace='handler')),
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
 ]
