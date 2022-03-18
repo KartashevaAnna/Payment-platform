65774dd5e5b2a55b2aae9a4fd8d5601d4deb0cb5
anna kartasheva <annakartashevamail@gmail.com>
2022-03-18 13:27:10 +0300

    positivedecimalfield added

--- payment_handler/handler/models.py
+++ payment_handler/handler/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from datetime import datetime
+from django.db.models import DecimalField
+from django.core.exceptions import ValidationError
+from django.utils.functional import cached_property
+from django.utils.translation import gettext_lazy as _
 
 
 User = get_user_model()
@@ -12,8 +15,24 @@ CURRENCY_CHOICES = (
 )
 
 
+def validate_positive(value):
+    if value < 0:
+        raise ValidationError(
+            _("%(value)s is not positive."),
+            params={"value": value}
+        )
+
+
+class PositiveDecimalField(DecimalField):
+    description = _("Positive decimal number")
+
+    @cached_property
+    def validators(self):
+        return super().validators + [validate_positive]
+
+
 class UserAccount(models.Model):
-    amount = models.DecimalField(decimal_places=4, max_digits=20),
+    amount = PositiveDecimalField(decimal_places=4, max_digits=20, default=0)
     currency = models.CharField(
         max_length=6,
         choices=CURRENCY_CHOICES,
